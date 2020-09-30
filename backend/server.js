e29400eeaeae8e22b30112c446534f3694df6328
Christof <bauer.christof@gmail.com>
2020-10-01 00:38:58 +0545

    Fix the backend... use json

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.static(__dirname + '/public'));
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 // app.use(fileupload());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }))
